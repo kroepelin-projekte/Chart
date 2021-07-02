ee13958090a40578804aad8153b7c1dbf59a9d79
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte.de>
2021-07-02 12:08:16 +0200

    Change ilias_max_version

--- plugin.php
+++ plugin.php
@@ -3,6 +3,6 @@
 $id = "chrt";
 $version = "2.0";
 $ilias_min_version = "6";
-$ilias_max_version = "7.999.999";
+$ilias_max_version = "7.999";
 $responsible = "KPG";
 $responsible_mail = "dev@kroepelin-projekte.de";
