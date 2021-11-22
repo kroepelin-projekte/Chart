26437aa2441e959952e3a9c703fded5701670838
Ioanna Mitroulaki <ioanna.mitroulaki@kroepelin-projekte>
2021-11-22 13:39:36 +0000

    Add files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '67aae2d78de5baac64effca9f9867b1d9c2d5d38',
+        'name' => 'kpg/chart',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kpg/chart' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '67aae2d78de5baac64effca9f9867b1d9c2d5d38',
+            'dev_requirement' => false,
+        ),
+    ),
+);
