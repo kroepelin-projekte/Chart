f0aadf05daeda91edef07ca72f924aca2d855a0f
Katrin Großkopf <katrin.grosskopf@kroepelin-projekte.de>
2025-07-17 11:39:24 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -4,5 +4,5 @@ $id = "chrt";
 $version = "5.1";
 $ilias_min_version = "8";
 $ilias_max_version = "9.999";
-$responsible = "KPG";
+$responsible = "Kröpelin Projekt GmbH";
 $responsible_mail = "support@kroepelin-projekte.de";
