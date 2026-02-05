8a7f215ca5d7593295e5b5c028bfbbe45a807d14
Daniel Kipp <daniel.kipp@kroepelin-projekte.de>
2026-02-05 15:50:15 +0100

    debuging

--- classes/class.ilChartPluginGUI.php
+++ classes/class.ilChartPluginGUI.php
@@ -1209,13 +1209,19 @@ class ilChartPluginGUI extends ilPageComponentPluginGUI
             }
 
             if (preg_match('/^value_dataset_(\d+)_category_(\d+)$/', $k, $m)) {
+                $ds = (int) $m[1];
                 $cat = (int) $m[2];
-                if (!isset($valid_categories[$cat])) {
+                if (!isset($valid_datasets[$ds]) || !isset($valid_categories[$cat])) {
                     unset($properties[$k]);
                 }
                 continue;
             }
         }
+
+        echo '<pre>';
+        var_dump($properties);
+        echo '</pre>';
+        exit;
         return $properties;
     }
 }
